2c950907a8b54fdb3e0dcd47ed1567bd5027e977
Matt Morris <double.emms@gmail.com>
2023-10-15 14:24:22 -0600

    extract a basic transformer class
    this is a first step towards a more object-oriented approach. it's not
    much of a change, but it gives us a place to start hanging things.

--- main.rb
+++ main.rb
@@ -3,48 +3,61 @@ require "date"
 
 input = CSV.read("input.csv", headers: true)
 
-# headers:
-# ["first_name", "last_name", "dob", "member_id", "effective_date", "expiry_date", "phone_number"]
+class Transformer
+  def initialize(row)
+    @row = row
+  end
+
+  def first_name
+    @row["first_name"]&.strip
+  end
+
+  def last_name
+    @row["last_name"]&.strip
+  end
+
+  def dob
+    Date.parse(@row["dob"]).iso8601
+  rescue
+    nil
+  end
+
+  def member_id
+    @row["member_id"]
+  end
+
+  def effective_date
+    Date.parse(@row["effective_date"]).iso8601
+  rescue
+    nil
+  end
+
+  def expiry_date
+    Date.parse(@row["expiry_date"]).iso8601
+  rescue
+    nil
+  end
+
+  def phone_number
+    @row["phone_number"].gsub(/\D/, "")
+  rescue
+    nil
+  end
+end
 
 CSV.open("output.csv", "w", headers: true) do |csv|
   csv << input.headers
 
   input.each do |row|
-    row["first_name"] = begin
-      row["first_name"]&.strip
-    rescue
-      row["first_name"]
-    end
-    row["last_name"] = begin
-      row["last_name"]&.strip
-    rescue
-      row["last_name"]
-    end
-    row["dob"] = begin
-      Date.parse.call(row["dob"]).iso8601
-    rescue
-      row["dob"]
-    end
-    row["member_id"] = begin
-      row["member_id"]&.strip
-    rescue
-      row["member_id"]
-    end
-    row["effective_date"] = begin
-      Date.parse.call(row["effective_date"]).iso8601
-    rescue
-      row["effective_date"]
-    end
-    row["expiry_date"] = begin
-      Date.parse.call(row["expiry_date"]).iso8601
-    rescue
-      row["expiry_date"]
-    end
-    row["phone_number"] = begin
-      row["phone_number"]&.gsub(/[^0-9]/, "")
-    rescue
-      row["phone_number"]
-    end
+    transformer = Transformer.new(row)
+
+    row["first_name"] = transformer.first_name
+    row["last_name"] = transformer.last_name
+    row["dob"] = transformer.dob
+    row["member_id"] = transformer.member_id
+    row["effective_date"] = transformer.effective_date
+    row["expiry_date"] = transformer.expiry_date
+    row["phone_number"] = transformer.phone_number
 
     csv << row
   end
