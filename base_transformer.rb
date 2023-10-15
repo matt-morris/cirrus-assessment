19dd331a297290248bfd480d4fae7e9fcd0fde50
Matt Morris <double.emms@gmail.com>
2023-10-15 15:12:05 -0600

    split out base transformer
    normally, i prefer composition over inheritance, but for things like
    this, i think there's an argument for this sort of pattern. it wouldn't
    matter much for a one-off, but how many times is an import script like
    this actually one and done?
    
    in my experience, between different clients, different vendors, (even
    different systems in-house), this sort of data mapping tends to be an
    ongoing thing, so building a toolkit to handle things in a common way
    can be a big win.

---
+++ base_transformer.rb
@@ -0,0 +1,74 @@
+require "csv"
+require "date"
+
+class BaseTransformer
+  def initialize(row)
+    @row = row
+  end
+
+  def first_name
+    @row["first_name"]&.strip
+  end
+
+  def last_name
+    @row["last_name"]&.strip
+  end
+
+  def dob
+    parse_date(@row["dob"])
+  end
+
+  def member_id
+    @row["member_id"]
+  end
+
+  def effective_date
+    parse_date(@row["effective_date"])
+  end
+
+  def expiry_date
+    parse_date(@row["expiry_date"])
+  end
+
+  def phone_number
+    parse_phone_number(@row["phone_number"])
+  end
+
+  private
+
+  def parse_phone_number(value)
+    cleaned_value = value.to_s.gsub(/\D/, "").sub(/^1/, "")
+    (cleaned_value.length == 10) ? "+1#{cleaned_value}" : nil
+  end
+
+  def parse_date(value)
+    [
+      ["%m-%d-%y", /^\d{1,2}-\d{1,2}-\d{2}$/], # 1-11-88
+      ["%m/%d/%y", /^\d{1,2}\/\d{1,2}\/\d{2}$/], # 01/11/88
+      ["%m-%d-%Y", /^\d{1,2}-\d{1,2}-\d{4}$/], # 01-11-1988
+      ["%m/%d/%Y", /^\d{1,2}\/\d{1,2}\/\d{4}$/], # 01/11/1988
+      ["%m-%d-%y", /^\d{1,2}-\d{1,2}-\d{4}$/], # 01-11-1988
+      ["%m/%d/%y", /^\d{1,2}\/\d{1,2}\/\d{2}$/], # 01/11/88
+      ["%d-%m-%y", /^\d{1,2}-\d{1,2}-\d{2}$/], # 11-01-88
+      ["%d/%m/%y", /^\d{1,2}\/\d{1,2}\/\d{2}$/], # 11/01/88
+      ["%d-%m-%Y", /^\d{1,2}-\d{1,2}-\d{4}$/], # 11-01-1988
+      ["%d/%m/%Y", /^\d{1,2}\/\d{1,2}\/\d{4}$/], # 11/01/1988
+      ["%Y-%m-%d", /^\d{4}-\d{2}-\d{2}$/], # 1988-01-11
+      ["%d-%m-%Y", /^\d{1,2}-\d{1,2}-\d{4}$/], # 11-01-1988
+      ["%Y/%m/%d", /^\d{4}\/\d{2}\/\d{2}$/], # 1988/01/11
+      ["%d/%m/%Y", /^\d{1,2}\/\d{1,2}\/\d{4}$/], # 11/01/1988
+      ["%B %d, %Y", /^[A-Za-z]+ \d{1,2}, \d{4}$/], # January 11, 1988
+      ["%b %d, %Y", /^[A-Za-z]{3} \d{1,2}, \d{4}$/], # Jan 11, 1988
+      ["%d %B, %Y", /^\d{1,2} [A-Za-z]+, \d{4}$/], # 11 January, 1988
+      ["%d %b, %Y", /^\d{1,2} [A-Za-z]{3}, \d{4}$/] # 11 Jan, 1988
+    ].each do |format, regex|
+      next unless value.to_s.match?(regex)
+      return Date.strptime(value.to_s, format).iso8601
+    rescue ArgumentError
+      next
+    rescue
+      nil
+    end
+    nil
+  end
+end
