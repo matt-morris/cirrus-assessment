4dad285b2d775f47cb5150fbce0200c95156c140
Matt Morris <double.emms@gmail.com>
2023-10-15 15:33:53 -0600

    set up initial specs

--- spec/importa_spec.rb
+++ spec/importa_spec.rb
@@ -1,11 +1,41 @@
 # frozen_string_literal: true
 
+class Transformer < Importa::BaseTransformer
+  def first_name
+    @row["first_name"]&.strip
+  end
+
+  def last_name
+    @row["last_name"]&.strip
+  end
+
+  def dob
+    parse_date(@row["dob"])
+  end
+
+  def member_id
+    @row["member_id"]
+  end
+
+  def effective_date
+    parse_date(@row["effective_date"])
+  end
+
+  def expiry_date
+    parse_date(@row["expiry_date"])
+  end
+
+  def phone_number
+    parse_phone_number(@row["phone_number"])
+  end
+end
+
 RSpec.describe Importa do
   it "has a version number" do
     expect(Importa::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "strips whitespace from stings" do
+    expect(Transformer.new({"first_name" => " John "}).first_name).to eq("John")
   end
 end
